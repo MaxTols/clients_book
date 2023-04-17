ff1a36144abd27e392cdceca791a42e40982f737
MaxTols <tolstikov-95@inbox.ru>
2023-04-17 10:38:15 +0300

    fisrt commit

--- main.py
+++ main.py
@@ -40,18 +40,18 @@ def add_phone(client_id, phone):
     conn.commit()
 
 
-def change_client(text, client_id, first_name=None, last_name=None, email=None, phone=None):
-    if text == 'phone':
+def change_client(colum, client_id, data):
+    if colum == 'phone':
         last_phone = (input('Введите предыдущий номер: '))
         cur.execute("""
         UPDATE phone SET phone=%s
         WHERE client_id=%s AND phone=%s;
-        """, (phone, client_id, last_phone))
+        """, (data, client_id, last_phone))
     else:
         cur.execute("""
-        UPDATE client SET first_name=%s, last_name=%s, email=%s 
-        WHERE client_id=%s;
-        """, (first_name, last_name, email, client_id))
+        UPDATE client SET %(colum)s='%(data)s'
+        WHERE client_id=%(id)s;
+        """ % {'colum': colum, 'data': data, 'id': client_id})
     print('Запись обновлена')
     conn.commit()
 
@@ -75,13 +75,12 @@ def delete_client(client_id):
     conn.commit()
 
 
-def find_client(first_name=None, last_name=None, email=None, phone=None):
-    cur.execute("""
-    SELECT * FROM client c
-    LEFT JOIN phone p ON c.client_id = p.client_id
-    WHERE first_name=%s OR last_name=%s OR email=%s OR phone=%s;
-    """, (first_name, last_name, email, phone))
-    pprint(cur.fetchall())
+def find_client(data):
+    user_data = set(data.split())
+    users = all_info_users()
+    for user in users:
+        if user_data.issubset(user):
+            pprint(user)
 
 
 def delete_table():
@@ -92,6 +91,14 @@ def delete_table():
     print('Таблицы удалены!')
 
 
+def all_info_users():
+    cur.execute("""
+    SELECT * FROM client c
+    LEFT JOIN phone p ON c.client_id = p.client_id;
+    """)
+    return cur.fetchall()
+
+
 def all_info():
     cur.execute("""
     SELECT * FROM client c
@@ -100,22 +107,23 @@ def all_info():
     pprint(cur.fetchall())
 
 
-with psycopg2.connect(database="clients_db", user="postgres", password="postgres") as conn:
-    with conn.cursor() as cur:
-        # create_db()
-        # add_client('Tony', 'Stark', 'stark@mail.ru')
-        # add_client('Steve', 'Rogers', 'capitan@america.ru')
-        # add_client('Bruce', 'Wayne', 'Batmen@inbox.com')
-        # add_phone(1, '+11111111111')
-        # add_phone(1, '+13333333333')
-        # add_phone(2, '+15555555555')
-        # change_client('phone', 1, phone='+19999999999')
-        # change_client('client', 1, first_name='Iron', last_name="Man", email='iron@ya.com')
-        # delete_phone(2, '+15555555555')
-        # delete_client(3)
-        # find_client(phone='+13333333333')
-        # find_client(first_name='Steve')
-        # delete_table()
-        all_info()
+if __name__ == '__main__':
+    with psycopg2.connect(database="clients_db", user="postgres", password="postgres") as conn:
+        with conn.cursor() as cur:
+            create_db()
+            add_client('Tony', 'Stark', 'stark@mail.ru')
+            add_client('Steve', 'Rogers', 'capitan@america.ru')
+            add_client('Bruce', 'Wayne', 'Batmen@inbox.com')
+            add_phone(1, '+11111111111')
+            add_phone(1, '+13333333333')
+            add_phone(2, '+15555555555')
+            change_client('phone', 1, '+19999999999')
+            change_client('last_name', 1, 'Sanders')
+            delete_phone(2, '+15555555555')
+            delete_client(3)
+            find_client('Tony')
+            find_client('+13333333333')
+            all_info()
+            # delete_table()
 
-conn.close()
+    conn.close()
